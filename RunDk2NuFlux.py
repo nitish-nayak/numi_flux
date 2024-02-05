ffe2a2573e3ae722614f5a9464021799636b725c
nitish-nayak <felarof.nayak@gmail.com>
2024-02-04 21:56:11 -0600

    ignore aclic files as well

--- RunDk2NuFlux.py
+++ RunDk2NuFlux.py
@@ -13,5 +13,7 @@ ROOT.gROOT.SetBatch(True)
 
 from ROOT import Dk2NuFlux
 
-f = Dk2NuFlux("/cvmfs/uboone.osgstorage.org/stash/uboonebeam/numi_dk2nu_zero_threshold/RHC/*_5*_0000.root")
-f.CalculateFlux("/uboone/data/users/bnayak/ppfx/flugg_studies/comparisons/dk2nu_rhc.root")
+#  f = Dk2NuFlux("/cvmfs/uboone.osgstorage.org/stash/uboonebeam/numi_dk2nu_zero_threshold/RHC/*_5*_0000.root")
+#  f.CalculateFlux("/uboone/data/users/bnayak/ppfx/flugg_studies/comparisons/dk2nu_rhc.root")
+f = Dk2NuFlux("/cvmfs/uboone.osgstorage.org/stash/uboonebeam/numi_dk2nu_zero_threshold/FHC/g4numiv6_minervame_me000z200i_2*root")
+f.CalculateFlux("/uboone/data/users/bnayak/ppfx/flugg_studies/comparisons/dk2nu_fhc.root")
