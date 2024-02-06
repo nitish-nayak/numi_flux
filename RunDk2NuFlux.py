eda2a8ab02f2323f9236043fb1bc6ba85551a86d
nitish-nayak <felarof.nayak@gmail.com>
2024-02-06 12:50:50 -0600

    Add in rhc changes in python script as well

--- RunDk2NuFlux.py
+++ RunDk2NuFlux.py
@@ -22,8 +22,8 @@ ROOT.gROOT.ProcessLine('#include "Dk2NuFlux.hh"')
 ROOT.gSystem.Load("Dk2NuFlux_cc.so")
 ROOT.gROOT.SetBatch(True)
 
-#  f = Dk2NuFlux("/cvmfs/uboone.osgstorage.org/stash/uboonebeam/numi_dk2nu_zero_threshold/RHC/*_5*_0000.root")
-#  f.CalculateFlux("/uboone/data/users/bnayak/ppfx/flugg_studies/comparisons/dk2nu_rhc.root")
-outfile="/uboone/data/users/bnayak/ppfx/flugg_studies/comparisons/dk2nu_fhc_ppfx.root"
-f = ROOT.Dk2NuFlux("/cvmfs/uboone.osgstorage.org/stash/uboonebeam/numi_dk2nu_zero_threshold/FHC/g4numiv6_minervame_me000z200i_2*.root", outfile)
+#  outfile="/uboone/data/users/bnayak/ppfx/flugg_studies/comparisons/dk2nu_fhc_ppfx.root"
+#  f = ROOT.Dk2NuFlux("/cvmfs/uboone.osgstorage.org/stash/uboonebeam/numi_dk2nu_zero_threshold/FHC/g4numiv6_minervame_me000z200i_2*.root", outfile)
+outfile="/uboone/data/users/bnayak/ppfx/flugg_studies/comparisons/dk2nu_rhc_ppfx.root"
+f = ROOT.Dk2NuFlux("/cvmfs/uboone.osgstorage.org/stash/uboonebeam/numi_dk2nu_zero_threshold/RHC/*_5*_0000.root", outfile)
 f.CalculateFlux()
