cdb9d31ef0342a17ac9f1a62a6898d0bc3abcfab
nitish-nayak <felarof.nayak@gmail.com>
2024-02-12 10:01:30 -0600

    delete old file

--- python/dk2nu_example.py
+++ python/dk2nu_example.py
@@ -8,7 +8,8 @@ ROOT.gROOT.SetBatch(True)
 
 #  outfile="/uboone/data/users/bnayak/ppfx/flugg_studies/comparisons/dk2nu_fhc_ppfx.root"
 #  f = ROOT.Dk2NuFlux("/cvmfs/uboone.osgstorage.org/stash/uboonebeam/numi_dk2nu_zero_threshold/FHC/g4numiv6_minervame_me000z200i_2*.root", outfile)
+#  f.CalculateFlux()
 
-outfile="/uboone/data/users/bnayak/ppfx/flugg_studies/comparisons/dk2nu_rhc_ppfx_test.root"
-f = ROOT.Dk2NuFlux("/cvmfs/uboone.osgstorage.org/stash/uboonebeam/numi_dk2nu_zero_threshold/RHC/*_599_0000.root", outfile)
+outfile="/uboone/data/users/bnayak/ppfx/flugg_studies/comparisons/test/dk2nu_rhc_ppfx_test.root"
+f = ROOT.Dk2NuFlux("/cvmfs/uboone.osgstorage.org/stash/uboonebeam/numi_dk2nu_zero_threshold/RHC/*_5*_0000.root", outfile)
 f.CalculateFlux()
--- python/flugg_example.py
+++ python/flugg_example.py
@@ -6,10 +6,10 @@ import ROOT
 ROOT.gSystem.Load("../lib/FluggFlux_cc.so")
 ROOT.gROOT.SetBatch(True)
 
-#  outfile='/uboone/data/users/bnayak/ppfx/flugg_studies/comparisons/flugg_rhc.root'
-#  f = FluggFlux("/uboone/data/users/bnayak/ppfx/flugg_studies/flugg_files/rhc/flugg*_70*.root", outfile)
-#  f.CalculateFlux()
-
-outfile='/uboone/data/users/bnayak/ppfx/flugg_studies/comparisons/flugg_fhc_test.root'
-f = ROOT.FluggFlux('/uboone/data/users/bnayak/ppfx/flugg_studies/flugg_files/fhc/flugg*_0000.root', outfile)
+outfile='/uboone/data/users/bnayak/ppfx/flugg_studies/comparisons/test/flugg_rhc_test.root'
+f = ROOT.FluggFlux("/uboone/data/users/bnayak/ppfx/flugg_studies/flugg_files/rhc/flugg*_70*.root", outfile)
 f.CalculateFlux()
+
+#  outfile='/uboone/data/users/bnayak/ppfx/flugg_studies/comparisons/flugg_fhc_test.root'
+#  f = ROOT.FluggFlux('/uboone/data/users/bnayak/ppfx/flugg_studies/flugg_files/fhc/flugg*_0000.root', outfile)
+#  f.CalculateFlux()
