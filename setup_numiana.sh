0435517addf54c423aa49047f21d2d797cfc125d
nitish-nayak <felarof.nayak@gmail.com>
2024-02-12 10:01:00 -0600

    rename setup script

---
+++ setup_numiana.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+setup(){
+
+    export MODE="NUMI"
+    export BOOSTROOT=${BOOST_DIR}/source/boost_1_66_0
+    #DK2NU:
+    export DK2NU_INC=${DK2NU}/include/dk2nu/tree
+    export DK2NU_LIB=${DK2NU}/lib
+
+    # setup for jobsub client
+    # according to the prescription in Mike Kirby's talk
+    # minerva doc-10551, Dec 2014 (same doc can be found for other experiments)
+    export NUMIANA_DIR=${PWD}
+    export NUMIANA_INC=${NUMIANA_DIR}/include
+    export LD_LIBRARY_PATH=${NUMIANA_DIR}/lib:${NUMIANA_DIR}/dict:${PPFX_DIR}/lib:$LD_LIBRARY_PATH
+}
+HOST=$(hostname -f)
+echo $HOST
+if echo "$HOST" | grep 'uboone';then
+    echo "executing for the $HOST"
+    setup
+else
+    echo "This is not a uboone machine. Sorry!"
+fi
+
