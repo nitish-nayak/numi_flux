f2c5bc3640879927f1a81d3444cf4c0d7aa91656
nitish-nayak <felarof.nayak@gmail.com>
2024-02-13 06:35:04 -0600

    some comments for output branches

--- include/Outputs.h
+++ include/Outputs.h
@@ -18,24 +18,24 @@ struct RootOutput
 
   void Write();
 
-  Float_t nuE       = -5.;
-  Float_t nudirX    = -5.;
-  Float_t nudirY    = -5.;
-  Float_t nudirZ    = -5.;
+  Float_t nuE       = -5.;          // neutrino energy
+  Float_t nudirX    = -5.;          // neutrino X direction
+  Float_t nudirY    = -5.;          // neutrino Y direction
+  Float_t nudirZ    = -5.;          // neutrino Z direction
 
-  Float_t wgt       = 1.;
-  Float_t wgt_ppfx  = 1.;
-  Int_t ptype       = -1000;
-  Int_t ncascade    = -1;
-  Int_t ntype       = -1000;
-  Int_t pmedium     = -1000;
-  Int_t decaytype   = -1000;
+  Float_t wgt       = 1.;           // weight at location, includes correction for muon polarization
+  Float_t wgt_ppfx  = 1.;           // ppfx CV weight when available (only for dk2nu)
+  Int_t ptype       = -1000;        // parent particle code. uses GEANT codes for flugg, PDG for dk2nu
+  Int_t ncascade    = -1;           // number of interactions before neutrino, 2 = primary, etc
+  Int_t ntype       = -1000;        // neutrino pdg code
+  Int_t pmedium     = -1000;        // material codes for neutrino parent projectiles (See MINOS docdb: 9070)
+  Int_t decaytype   = -1000;        // type of decay (See MINOS docdb: 9070)
 
-  Float_t pE        = -5.;
-  Float_t pPt       = -5.;
-  Float_t pPz       = -5.;
-  Float_t pTheta    = -5.;
-  Float_t pxF       = -5.;
+  Float_t pE        = -5.;          // parent energy in lab frame
+  Float_t pPt       = -5.;          // parent pT in lab frame
+  Float_t pPz       = -5.;          // parent pZ in lab frame
+  Float_t pTheta    = -5.;          // parent angle (in radians) wrt beamline (z-axis)
+  Float_t pxF       = -5.;          // parent Feynman-x, only available for primary in flugg, dk2nu has for any parent
 
   TFile* fout;
 
