35b789436c2f21a861c1092e6422fb401f05d4f2
nitish-nayak <felarof.nayak@gmail.com>
2024-03-18 23:11:24 -0500

    add jobsub integration

--- jobsub/numiana_job.sh
+++ jobsub/numiana_job.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Make box around text @climagic
+function box() { t="$1xxxx";c=${2:-=}; echo ${t//?/$c}; echo "$c $1 $c"; echo ${t//?/$c}; }
+
 function setup_localppfx {
     cd $CONDOR_DIR_INPUT
     mkdir -p "$CONDOR_DIR_INPUT"/local_ppfx
@@ -16,21 +19,23 @@ function setup_localppfx {
     cd $CONDOR_DIR_INPUT
 }
 
-
-echo "At CONDOR_DIR_INPUT"
+box "At CONDOR_DIR_INPUT"
 cd $CONDOR_DIR_INPUT
 pwd
 ls
 
-echo "Untarring numi_flux"
+echo
+box "Untarring numi_flux"
 tar xvfz local_numiflux.tar.gz -C ./ > /dev/null
 
-echo "Setting up various dependencies"
+echo
+box "Setting up various dependencies"
 source /cvmfs/uboone.opensciencegrid.org/products/setup_uboone_mcc9.sh
 setup uboonecode v08_00_00_76 -q e17:prof
 
 if [ -f local_ppfx.tar.gz ]; then
-    echo "Setting up local PPFX"
+    echo
+    box "Setting up local PPFX"
     setup_localppfx
     echo "PPFX_DIR : "$PPFX_DIR
     if [[ -z "${PPFX_DIR}" ]]; then
@@ -39,16 +44,31 @@ if [ -f local_ppfx.tar.gz ]; then
     fi
 fi
 
-echo "Setting up local numi_flux repo"
+echo
+box "Setting up local numi_flux repo and building dictionaries"
 source setup_numiana_grid.sh
+make all
 
 if [[ ! -f "${MACRO}" ]]; then
     echo "No python macro found! Exiting.."
     exit
 fi
 
-echo "Running python macro"
-python "${MACRO}" "${FILELIST}"
+echo
+FILELIST="input_files.txt"
+box "The input file list is"
+cat "$FILELIST"
+echo "Splitting filelist into chunks of 1 file"
+cat "${FILELIST}" | split -l 1 -a 3 -d
+job_filelist="x"`printf "%03d" "${PROCESS}"`
 
-echo "Copying output file"
-mv numi_flux_output.root "${CONDOR_DIR_NUMIANA}"/numi_flux_output_"${PROCESS}".root
+box "Filelist for job is : "
+cat "$job_filelist"
+
+echo
+box "Running python macro for process : "${PROCESS}
+python "${MACRO}" "${job_filelist}" "${SEED}"
+
+echo
+box "Copying output file"
+mv numi_flux_output.root "${CONDOR_DIR_NUMIANA}"/numi_flux_output_"${PROCESS}"_seed"${SEED}".root
