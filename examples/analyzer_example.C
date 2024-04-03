b0dafeff504ddc3d3ac4714da107496d68d65034
Nitish Nayak <felarof.nayak@gmail.com>
2024-04-03 15:40:34 -0400

    preliminary class for analyzing my saved ttrees using experimental root features for implicit parallelism

---
+++ examples/analyzer_example.C
@@ -0,0 +1,68 @@
+#include "../include/Analyzer.h"
+
+using namespace ROOT;
+using namespace ROOT::RDF;
+using namespace NuMI;
+
+void analyzer_example(std::string pattern, unsigned int nthreads = 1)
+{
+  nthreads > 1 ? ROOT::EnableImplicitMT(nthreads) : void();
+
+  TChain chain("outTree");
+  chain.Add(pattern.c_str());
+  RDataFrame df(chain);
+
+  TH1D* dummy_flux = new TH1D("flux", "#nu_{#mu} Flux", 100, 0, 5);
+
+  // define my histogram helper class
+  Ana1DHelper helper_cv = Ana1DHelper(*dummy_flux, nthreads);
+  Ana1DHelper helper = Ana1DHelper(*dummy_flux, nthreads);
+
+  // define my selections and apply it to my data
+  auto selNumu = [](int ntype){ return ntype == 14; };
+  std::cout << "Filtering.." << std::endl;
+  auto f_df = df.Filter(selNumu, {"ntype"});
+
+  // define my weights
+  auto cvWgt = [](float wgt, float wgt_ppfx){ return wgt*wgt_ppfx; };
+  f_df = f_df.Define("flux_cv_wgt", cvWgt, {"wgt", "wgt_ppfx"});
+
+  // the move is important because the helper class should not survive the event loop
+  RResultPtr<TH1D> r_numu_cv = f_df.Book<float, float>(std::move(helper_cv), {"nuE", "flux_cv_wgt"});
+  RResultPtr<TH1D> r_numu = f_df.Book<float, float>(std::move(helper), {"nuE", "wgt"});
+
+  // fancy indexing is possible but I haven't implemented handling for it yet
+  const int Nunivs = 10;
+  // auto univWgts = [&Nunivs](float wgt, const RVecF &wgt_ppfxunivs){ return wgt*VecOps::Take(wgt_ppfxunivs, Nunivs); }
+  std::vector<Ana1DHelper> helper_univs;
+  for(int iUniv = 0; iUniv < Nunivs; iUniv++)
+      helper_univs.emplace_back(*dummy_flux, nthreads);
+
+
+  // book the action
+  std::cout << "Booking univs.." << std::endl;
+  std::vector<RResultPtr<TH1D>> r_numu_univs;
+  for(int iUniv = 0; iUniv < Nunivs; iUniv++){
+    auto univWgt = [iUniv](float wgt, const RVecF &wgt_ppfxunivs){ return wgt*wgt_ppfxunivs[iUniv]; };
+    f_df = f_df.Define("flux_univ"+std::to_string(iUniv)+"_wgt",
+                       univWgt, {"wgt", "wgt_ppfxunivs"});
+
+    auto r_numu_univ = f_df.Book<float, float>(std::move(helper_univs[iUniv]),
+                                               {"nuE", "flux_univ"+std::to_string(iUniv)+"_wgt"});
+    r_numu_univs.emplace_back(r_numu_univ);
+  }
+
+  // booking is all lazy, the event loop on the dataframe is triggered by trying to access the pointer (for everything)
+  std::cout << "CV Flux : " << r_numu_cv->Integral() << std::endl;
+  std::cout << "Raw Flux : " << r_numu->Integral() << std::endl;
+
+  // the histograms now exist so we can save them
+  TFile* fout = new TFile("numu_fluxes.root", "recreate");
+  r_numu_cv->Write("numu_cv");
+  r_numu->Write("numu");
+  fout->mkdir("univs"); fout->cd("univs");
+  for(int i = 0; i < Nunivs; i++)
+    r_numu_univs[i]->Write(TString::Format("numu_univ%d", i));
+
+  fout->Close();
+}
---
+++ include/Analyzer.h
@@ -0,0 +1,86 @@
+#ifndef Analyzer_h
+#define Analyzer_h
+
+#pragma once
+
+#include "Outputs.h"
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+#include <ROOT/RDataFrame.hxx>
+#include <memory>
+
+namespace NuMI {
+
+  static int AnaID;
+  class Ana1DHelper : public ROOT::Detail::RDF::RActionImpl<Ana1DHelper>
+  {
+  public:
+    // we need these
+    using Result_t = TH1D;
+  private:
+    std::vector<std::shared_ptr<TH1D>> fHistos; // one per data processing slot
+    std::shared_ptr<TH1D> fHistoResult;
+    const unsigned int fThreads;
+
+  public:
+    Ana1DHelper(const TH1D& dummy, const unsigned int nThreads=1)
+      : fThreads(nThreads)
+    {
+      AnaID++;
+      const unsigned int nSlots = ROOT::IsImplicitMTEnabled() ? fThreads : 1;
+
+      const double nbins = dummy.GetNbinsX();
+      const double xlow  = dummy.GetBinLowEdge(1);
+      const double xhigh = dummy.GetBinLowEdge(nbins+1);
+      fHistoResult = std::make_shared<TH1D>(TString::Format("%s_id%d_result", dummy.GetName(), AnaID), dummy.GetTitle(),
+                                            nbins, xlow, xhigh);
+
+      for(unsigned int i = 0; i < nSlots; i++) {
+        fHistos.emplace_back(std::make_shared<TH1D>(
+                             TString::Format("%s_id%d_slot%d", dummy.GetName(), AnaID, i), dummy.GetTitle(),
+                             nbins, xlow, xhigh)
+        );
+      }
+    }
+    // don't allow copies
+    Ana1DHelper(Ana1DHelper &&) = default;
+    Ana1DHelper(const Ana1DHelper &) = delete;
+
+    std::shared_ptr<TH1D> GetResultPtr() const { return fHistoResult; }
+
+    // we need these for df.Book()
+    void Initialize() {}
+    void InitTask(TTreeReader *, unsigned int) {}
+
+    template <typename... ColumnTypes>
+    void Exec(unsigned int slot, ColumnTypes... values)
+    {
+      // first one is val, second one is weight
+      std::array<double, sizeof...(ColumnTypes)> valuesArr{static_cast<double>(values)...};
+      if(valuesArr.size() == 1)
+        fHistos[slot]->Fill(valuesArr[0]);
+      else if(valuesArr.size() == 2)
+        fHistos[slot]->Fill(valuesArr[0], valuesArr[1]);
+      else{
+       std::cerr << "Expect two columns as input!! Exiting!" << std::endl;
+       std::exit(1);
+      }
+    }
+
+    void Finalize()
+    {
+      for(unsigned int i = 0; i < fHistos.size(); i++)
+        fHistoResult->Add(fHistos[i].get());
+    }
+
+    std::string GetActionName(){
+      return "Ana1DHelper";
+    }
+  };
+  // end helper
+}
+
+#endif
